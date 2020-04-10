6f72186cc414b1617543d3166f5ea8a127b83506
Jeremy Parker <jeremyeparker@yahoo.com>
2020-04-09 21:13:09 -0400

    reorganize

---
+++ Completed/tempConversion.js
@@ -0,0 +1,13 @@
+//Celsius times 9/5, plus 32. formula for conversion of celsuis to fahrenheit
+
+function convertToF(celsius) {
+
+    let fahrenheit = celsius * (9/5) + 32;
+
+    console.log(fahrenheit);
+    
+    return fahrenheit;
+  }
+  
+  convertToF(0);
+  
