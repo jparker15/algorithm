ab71d518bb742897b07cc8bcd39a75feff0c4b53
Jeremy Parker <jeremyeparker@yahoo.com>
2020-05-05 17:11:31 -0400

    start of algorithm

---
+++ inProgress/booWho.js
@@ -0,0 +1,27 @@
+/**
+ 
+Check if a value is classified as a boolean primitive.
+Return true or false.
+Boolean primitives are true and false.} bool 
+ */
+
+
+function booWho(bool) {
+    return bool;
+  }
+  
+  booWho(null);
+  
+
+  /**
+   * booWho(true) should return true.
+booWho(false) should return true.
+booWho([1, 2, 3]) should return false.
+booWho([].slice) should return false.
+booWho({ "a": 1 }) should return false.
+booWho(1) should return false.
+booWho(NaN) should return false.
+booWho("a") should return false.
+booWho("true") should return false.
+booWho("false") should return false.
+   */
