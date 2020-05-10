0a5f2447acb63c540568b7a7e8959d4e0cf8b294
Jeremy Parker <jeremyeparker@yahoo.com>
2020-05-09 23:42:35 -0400

    add test cases

--- inProgress/sliceSplice.js
+++ inProgress/sliceSplice.js
@@ -10,8 +10,22 @@ Return the resulting array. The input arrays should remain the same after the fu
  */
 
 function frankenSplice(arr1, arr2, n) {
+
+    let a1 = arr1.slice(0);
+
+    console.log(a1);
+    
+
     return arr2;
   }
   
   frankenSplice([1, 2, 3], [4, 5, 6], 1);
   
+  /**
+   * frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
+frankenSplice([1, 2], ["a", "b"], 1) should return ["a", 1, 2, "b"].
+frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2) should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
+All elements from the first array should be added to the second array in their original order.
+The first array should remain the same after the function runs.
+The second array should remain the same after the function runs.
+   */
