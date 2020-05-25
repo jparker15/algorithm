8342d6f0f446afe2c48079d857436be4f9e42429
Jeremy Parker <jeremyeparker@yahoo.com>
2020-05-24 23:13:57 -0400

    array sorted in accessending order, start of conditionals

--- inProgress/whereDoIBelong.js
+++ inProgress/whereDoIBelong.js
@@ -16,10 +16,24 @@ Likewise, getIndexToIns([20,3,5], 19)
 
 
 function getIndexToIns(arr, num) {
+  
+    arr.sort((a,b) => a-b);
+  
+    for (let i = 0; i < arr.length; i++) {
+      
+      if (arr[i] <= num){
+        console.log(i);
+        
+      }
+      // else if (arr[i] < num){
+
+      // }
+      
+    }
     return num;
   }
   
-  getIndexToIns([40, 60], 50);
+  getIndexToIns([2, 5, 10], 15);
   
 
  /**TEST CASES
