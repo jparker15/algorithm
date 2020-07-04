0969f91aeed12e8d527c23625029ace0a3fe720d
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-04 00:00:48 -0400

    add prototype.area, extend Square class

--- hackerRank/day5-inheritance.js
+++ hackerRank/day5-inheritance.js
@@ -1,5 +1,5 @@
 class Rectangle {
-    constructor(w, h) {
+    constructor(h, w) {
         this.w = w;
         this.h = h;
         
@@ -7,18 +7,39 @@ class Rectangle {
     
 }
 
+
 /*
  *  Write code that adds an 'area' method to the Rectangle class' prototype
  */
-Rectangle.prototype.a = () =>{
-    
+Rectangle.prototype.area = function() {
+
+   if (this.w == undefined){
+       return this.h **2
+   }
+   return this.h*this.w
 }
+
+
 /*
  * Create a Square class that inherits from Rectangle and implement its class constructor
  */
 
- class Square extends Rectangle {
-     constructor(w,h){
-         super(w,h);
-     }
- }
+// class Square extends Rectangle {
+//     constructor(h,w) {
+//         super(h,w);
+//     }
+// }
+class Square extends Rectangle {
+    // only need one side to make a square
+    constructor(s) {
+        // super(s,s) = Rectangle constructor (h,w)
+        super(s,s);
+    }
+}
+
+let rec = new Rectangle(3,4)
+let sqr = new Square(3)
+
+ console.log(rec.area())
+ console.log(sqr.area())
+ 
