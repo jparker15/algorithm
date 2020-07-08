6d55b1cc193eed712923518480689dfe96fdbdd7
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-07 21:36:20 -0400

    init creation of layout

---
+++ hackerRank/day6-jsdates.js
@@ -0,0 +1,17 @@
+/**
+ * Given a date string, , in the format MM/DD/YYYY, find and return the day name for that date. 
+ * Each day name must be one of the following strings:
+ * Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday. 
+ * For example, the day name for the date 12/07/2016 is Wednesday.
+ * 
+ */
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+
+    
+    
+    return dayName;
+}
