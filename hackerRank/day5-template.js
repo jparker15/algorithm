7815b261640ca7274685c14a9aeb50594adfe3f0
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-07 21:35:57 -0400

    start

---
+++ hackerRank/day5-template.js
@@ -0,0 +1,12 @@
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    
+}
