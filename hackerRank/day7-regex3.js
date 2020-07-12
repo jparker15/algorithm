a9149ab684db553aff7c442770cacc082e7700c3
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-11 22:26:59 -0400

    global flag to complete

--- hackerRank/day7-regex3.js
+++ hackerRank/day7-regex3.js
@@ -3,7 +3,7 @@ function regexVar() {
      * Declare a RegExp object variable named 're'
      * It must match ALL occurrences of numbers in a string.
      */
-    const re = /(\d)+/;
+    const re = /(\d)+/g;
     
     /*
      * Do not remove the return statement
