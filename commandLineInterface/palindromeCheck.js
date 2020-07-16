07dc01bde0780d87db0d1a9d456c806ae05ad3c6
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-16 13:25:36 -0400

    reorganize lines, add restart function and rl question

--- commandLineInterface/palindromeCheck.js
+++ commandLineInterface/palindromeCheck.js
@@ -11,31 +11,9 @@ You'll need to remove all non-alphanumeric characters (punctuation, spaces and s
 We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 
 We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
- */
-const readline = require("readline"), 
-    
-    rl = readline.createInterface({
 
-        input: process.stdin,
-        output: process.stdout,
-        prompt: "OHAI>"
-
-    });
-
-
-
-
-function palindrome(str) {
-
-  // rl.question("enter a string check if its a palidrome true or false\nstring:", (str)=>{
-
-
-     
-  //   console.log(`${str} is a ${bool} `);
-  //   rl.close()
-  // });
-  str = str.toLowerCase();
-  console.log(str);
+Solution ##
+  // console.log(str);
 
   //remove all non-alphanumeric characters (punctuation, spaces and symbols)'
   /**
@@ -52,19 +30,63 @@ function palindrome(str) {
 
   //str = str.replace(/\s/g,"");
 
-  str = str.replace(/[\W_\s]/g,"");
+  // str = str.replace(/[\W_\s]/g,"");
     //reversed string
-  const pal = str.split("").reverse().join("");
+  // const pal = str.split("").reverse().join("");
 
-  console.log(pal,str)
+  // console.log(pal,str)
   
-  if (pal != str){
-    return false
-  }
-  return true
-  
-   
+  // if (pal != str){
+  //   return false
+  // }
+  // return true
+
+
+const readline = require("readline"), 
+    
+    rl = readline.createInterface({
+
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "OHAI>"
+
+    });
+
+const palindrome = (str) => {
+
+  rl.question("enter a string check if its a palindrome true or false\nstring:", (str)=>{
+
+    str = str.toLowerCase();
+
+    str = str.replace(/[\W_\s]/g,"");
+
+    const pal = str.split("").reverse().join("");
+
+    if (pal != str){
+      console.log(`${str} is not a palindrome`);
+      
+    }else{
+      console.log(`${str} is a palindrome!`);
+    }
+
+    rl.question(`Would you like to check another palindrome?\n(Yes or No)\ninput: `, restart);
+      
+  });
 }
-  
-  
-console.log(palindrome("2_A3*3#A2"))
+
+
+const restart = (answer) => {
+
+  answer = answer.toLowerCase().substring(0,1);
+
+  if (answer == "y" ){
+    rl.question("enter a string check if it's a palindrome true or false\nstring:", palindrome);
+  }
+  else {
+    rl.close();
+  }
+
+}
+
+
+palindrome()
