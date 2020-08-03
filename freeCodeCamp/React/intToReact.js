dc0e27717f444104683c86fdf846a82c1f7465c5
jeremy <jeremyeparker@yahoo.com>
2020-08-02 23:01:45 -0400

    composed child component to parent

--- freeCodeCamp/React/intToReact.js
+++ freeCodeCamp/React/intToReact.js
@@ -102,14 +102,51 @@ class MyComponent extends React.Component {
     render() {
       // change code below this line
   
-    const JSX =  <div>
-      <h1>Hello React!</h1>
-      </div>
+    // const JSX =  <div>
+    //   <h1>Hello React!</h1>
+    //   </div>
   
-      return JSX
+    //   return JSX
+
+    return(
+        <div>
+          <h1>Hello React!</h1>
+        </div>
+  
+    )
   
   
       // change code above this line
+    }
+  };
+
+/**
+ * Create a component with composition
+ */
+
+const ChildComponent = () => {
+    return (
+      <div>
+        <p>I am the child</p>
+      </div>
+    );
+  };
+  
+  class ParentComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>I am the parent</h1>
+          { /* change code below this line */ }
+
+          <ChildComponent />
+  
+          { /* change code above this line */ }
+        </div>
+      );
     }
   };
   
