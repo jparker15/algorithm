c123cc0598d92e4b480978d50427ea2e508e98fa
jeremy <jeremyeparker@yahoo.com>
2020-08-02 22:00:54 -0400

    add ReactDom.render

--- freeCodeCamp/React/intToReact.js
+++ freeCodeCamp/React/intToReact.js
@@ -44,4 +44,16 @@ React combines HTML with JavaScript functionality to create its own markup langu
             {/*JSX comment */}
         </div>
     );
-    
+
+/**
+ * Render HTML elems to the DOM
+ */
+    const JSX = (
+        <div>
+        <h1>Hello World</h1>
+        <p>Lets render this to the DOM</p>
+        </div>
+    );
+    // change code below this line
+    ReactDOM.render(JSX,document.getElementById("challenge-node"));
+
