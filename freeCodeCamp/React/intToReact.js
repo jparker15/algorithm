4358d2cb569e1e0465f773d0752b346d14477085
Jeremy Parker <jeremyeparker@yahoo.com>
2020-08-06 23:15:15 -0400

    add compose react component

--- freeCodeCamp/React/intToReact.js
+++ freeCodeCamp/React/intToReact.js
@@ -196,3 +196,41 @@ class TypesOfFood extends React.Component {
     );
   }
 };
+
+/**
+ * Compose React Components 
+ */
+
+class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        { /* change code below this line */ }
+
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+     super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        { /* change code below this line */ }
+
+        { /* change code above this line */ }
+        <Vegetables />
+      </div>
+    );
+  }
+};
+
