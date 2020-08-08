2f6bb7dfd2f590b593656ad85adc594ae8a15e76
jeremy <jeremyeparker@yahoo.com>
2020-08-08 19:06:12 -0400

    pass an array as props.tasks

--- freeCodeCamp/React/intToReact.js
+++ freeCodeCamp/React/intToReact.js
@@ -308,3 +308,31 @@ class Calendar extends React.Component {
     );
   }
 };
+
+/**
+ * Pass an Array as Props
+ */
+const List = (props) => {
+  { /* change code below this line */ }
+  return <p>{props.tasks.join(", ")}</p>
+  { /* change code above this line */ }
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        { /* change code below this line */ }
+        <List tasks={["walk dog", "buy grocereis"]}/>
+        <h2>Tomorrow</h2>
+        <List tasks={["dog walk", "burn groec", "lorem"]}/>
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
