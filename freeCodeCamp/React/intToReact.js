7d299dd1d90e62371d9ea0da6773d3023729f2a1
Jeremy Parker <jeremyeparker@yahoo.com>
2020-08-09 23:50:25 -0400

    add review using props

--- freeCodeCamp/React/intToReact.js
+++ freeCodeCamp/React/intToReact.js
@@ -440,3 +440,22 @@ class ResetPassword extends React.Component {
     );
   }
 };
+
+/**
+ * Review Using Props with Stateless Functional Components
+ */
+
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// change code below this line
+
