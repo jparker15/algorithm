c41204f3a78031c92a14ed703a5a377ae1e57294
Jeremy Parker <jeremyeparker@yahoo.com>
2020-08-12 23:43:14 -0400

    review using props with stateless function comps

--- freeCodeCamp/React/intToReact.js
+++ freeCodeCamp/React/intToReact.js
@@ -459,3 +459,14 @@ class CampSite extends React.Component {
 };
 // change code below this line
 
+const Camper = props => 
+  <p>{props.name}</p>
+
+
+Camper.defaultProps = {
+  name: "CamperBot"
+}
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired
+}
