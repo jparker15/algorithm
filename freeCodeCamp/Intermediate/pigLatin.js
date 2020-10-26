709511581dffe0444fb3ab6c838e37eff4d32d34
Jar <jeremyeparker@yahoo.com>
2020-10-25 23:59:18 -0400

    add regex

--- freeCodeCamp/Intermediate/pigLatin.js
+++ freeCodeCamp/Intermediate/pigLatin.js
@@ -12,6 +12,18 @@ Translate the provided string to Pig Latin. Input strings are guaranteed to be E
  */
 
 function translatePigLatin(str) {
+    const consonant = /^[aeiou]/g;
+    const vowel = /[aeiou]/g;
+
+    console.log(vowel);
+
+    for (let i = 0; i < str.length; i++) {
+        
+        if(str[i].match(consonant)){
+            console.log(str[i]);
+        }
+        
+    }
     return str;
   }
   
