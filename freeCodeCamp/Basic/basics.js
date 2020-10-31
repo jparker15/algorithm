d6e7e9252d981f751184a9db6e8e781f23e94fbe
Jeremy <jeremyeparker@yahoo.com>
2020-10-30 23:54:07 -0400

    add largest of four

--- freeCodeCamp/Basic/basics.js
+++ freeCodeCamp/Basic/basics.js
@@ -60,3 +60,10 @@ const longestWord = (str) =>{
 }
 
 console.log(longestWord("The quick brown fox jumped over the lazy dog"));
+
+const largestOfFour = (arr) => {
+
+  console.log(arr)
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
