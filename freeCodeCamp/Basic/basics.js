6b15dc6be8736bc0450852029b1c99dce92526a8
Jeremy <jeremyeparker@yahoo.com>
2021-01-09 23:42:56 -0500

    roman numeral converter

--- freeCodeCamp/Basic/basics.js
+++ freeCodeCamp/Basic/basics.js
@@ -220,6 +220,9 @@ const randomWholeNum =() => {
 }
 
 const convertToRoman = (num) => {
+
+  let decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let romanNumeral = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
   return num;
  }
  
