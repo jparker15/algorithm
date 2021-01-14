ea312ce9bfe3e9c87995b960d02c02dbf4f06a85
Jeremy <jeremyeparker@yahoo.com>
2021-01-13 23:50:26 -0500

    set arrays to object

--- freeCodeCamp/Basic/basics.js
+++ freeCodeCamp/Basic/basics.js
@@ -221,8 +221,7 @@ const randomWholeNum =() => {
 
 const convertToRoman = (num) => {
 
-  let decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  let romanNumeral = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+  let conversion = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
   return num;
  }
  
