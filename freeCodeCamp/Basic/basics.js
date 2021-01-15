bb3f2618068e99252aef57cdbc5e39f2dac24e47
Jeremy <jeremyeparker@yahoo.com>
2021-01-14 22:19:26 -0500

    rev string algo

--- freeCodeCamp/Basic/basics.js
+++ freeCodeCamp/Basic/basics.js
@@ -5,3 +5,16 @@ const convertToF = (celsius) =>{
 }
 
 convertToF(-30);
+
+const reverseString = (str) => {
+  let revStr = ""
+  for (let i = str.length - 1; i >= 0; i--) {
+    
+    revStr += str[i];
+    
+  }
+  console.log(revStr);
+  return str;
+}
+
+reverseString("hello");
