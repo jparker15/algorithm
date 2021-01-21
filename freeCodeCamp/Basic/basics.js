66c3d62889a27e7e9b6270d3c0ea04e6edfef171
Jeremy <jeremyeparker@yahoo.com>
2021-01-20 22:54:31 -0500

    confirm the ending of a string

--- freeCodeCamp/Basic/basics.js
+++ freeCodeCamp/Basic/basics.js
@@ -80,3 +80,9 @@ const largestOfFour = (arr) => {
 }
 
 largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]);
+
+const confirmEnding = (str, target) => {
+  return str;
+}
+
+confirmEnding("Bastian", "n");A
