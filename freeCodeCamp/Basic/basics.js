66ac84b8eb06cc12c8bbefacb9115260fcc530ed
Jeremy <jeremyeparker@yahoo.com>
2021-01-24 19:06:25 -0500

    truncate a Str

--- freeCodeCamp/Basic/basics.js
+++ freeCodeCamp/Basic/basics.js
@@ -115,3 +115,9 @@ const repeatStringNumTimes = (str, num)=> {
 }
 
 repeatStringNumTimes("*", 3);
+
+const truncateString = (str, num) => {
+  return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
