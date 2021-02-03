95d6d5b39a38659e9e4783a26cc37267d4474cbc
Jar <jeremyeparker@yahoo.com>
2021-02-02 22:06:56 -0500

    slice and splice

--- freeCodeCamp/Basic/basics.js
+++ freeCodeCamp/Basic/basics.js
@@ -177,3 +177,14 @@ const titleCase = (str)  =>{
 }
 
 titleCase("sHoRt AnD sToUt");
+/**
+You are given two arrays and an index.
+Copy each element of the first array into the second array, in order.
+Begin inserting elements at index n of the second array.
+Return the resulting array. The input arrays should remain the same after the function runs.
+ */
+const frankenSplice = (arr1, arr2, n) => {
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
