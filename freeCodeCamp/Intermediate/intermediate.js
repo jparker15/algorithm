930c0adea041e90dca1abdaed83f922e8100adce
Jeremy Parker <jeremyeparker@yahoo.com>
2021-02-16 23:52:26 -0500

    sum of all ints

---
+++ freeCodeCamp/Intermediate/intermediate.js
@@ -0,0 +1,9 @@
+/**. Return the sum of those two numbers plus the sum of all the numbers between them.
+ *  The lowest number will not always come first.
+For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10. */
+
+const sumAll = (arr) => {
+    return 1;
+  }
+  
+  sumAll([1, 4]);
