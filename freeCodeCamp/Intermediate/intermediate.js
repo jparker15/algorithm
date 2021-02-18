0eedb926fd0d2a929918e9fc0756e19eb2de69e0
Jeremy Parker <jeremyeparker@yahoo.com>
2021-02-17 23:54:08 -0500

    diff of two arrs

--- freeCodeCamp/Intermediate/intermediate.js
+++ freeCodeCamp/Intermediate/intermediate.js
@@ -7,3 +7,11 @@ const sumAll = (arr) => {
   }
   
   sumAll([1, 4]);
+/**Compare two arrays and return a new array with any items only found in one of the two given arrays, 
+ * but not both. In other words, return the symmetric difference of the two arrays. */
+  const diffArray = (arr1, arr2) =>{
+    var newArr = [];
+    return newArr;
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
