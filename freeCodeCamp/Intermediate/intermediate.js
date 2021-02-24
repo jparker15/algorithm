b2f16bbd25b1eba2fc41e8673dec29fdcd39e91a
Jeremy Parker <jeremyeparker@yahoo.com>
2021-02-23 23:50:47 -0500

    where 4 art thou

--- freeCodeCamp/Intermediate/intermediate.js
+++ freeCodeCamp/Intermediate/intermediate.js
@@ -21,3 +21,13 @@ function destroyer(arr) {
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+function whatIsInAName(collection, source) {
+  var arr = [];
+  // Only change code below this line
+
+
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
