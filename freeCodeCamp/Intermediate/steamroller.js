84b800922b454165fbe8565f78ee1f6414e90641
Jeremy <jeremyeparker@yahoo.com>
2021-04-23 21:58:50 -0400

    itterate arr

--- freeCodeCamp/Intermediate/steamroller.js
+++ freeCodeCamp/Intermediate/steamroller.js
@@ -1,4 +1,9 @@
-function steamrollArray(arr) {
+const steamrollArray = (arr) =>{
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+    
+  }
     return arr;
   }
   
